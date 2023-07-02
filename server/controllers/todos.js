ab6f04f22c8accfec2b4a51955933e6d5ee3d908
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 11:56:32 +0300

    add remaining functions such as delete, update etc.

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,6 +1,6 @@
 const toDo = require('../models/Todo')
 
-exports.toDo_create_post = async (req, res) => {
+exports.toDo_create_post = (req, res) => {
     let { title, description, due } = req.body
     const owner = req.user.id;
     const newTodo = new toDo({title, description, due, completed: false, owner})
@@ -11,5 +11,52 @@ exports.toDo_create_post = async (req, res) => {
     .catch((err) => {
         console.log(err)
         res.json({ message: "An error occured" })
+    })
+}
+
+exports.toDo_all_get = (req, res) => {
+    toDo.find({ owner: req.user.id })
+    .then((todos) => {
+        res.json({todos})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.json({ message: "An error occured"})
+    })
+}
+
+exports.toDo_single_get = (req, res) => {
+    const { id } = req.params
+    toDo.findById({_id : id})
+    .then((todo) => {
+        res.json({todo})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.json({message: "An error occured"})
+    })
+}
+
+exports.toDo_delete_delete = (req, res) => {
+    const { id } = req.params
+    toDo.findByIdAndDelete({ _id : id })
+    .then((todos) => {
+        res.json({todos})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.json({message: "An error occured"})
+    })
+}
+
+exports.toDo_update_patch = (req, res) => {
+    const { id } = req.params
+    toDo.findByIdAndUpdate({_id: id}, req.body)
+    .then((todo) => {
+        res.json({todo})
+    })
+    .catch((error) => {
+        console.log(error)
+        res.json({message: "An error occured"})
     })
 }
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,6 +5,10 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 const todoCtrl = require('../controllers/todos')
 
+router.get("/all", isLoggedIn, todoCtrl.toDo_all_get)
+router.get("/:id", isLoggedIn, todoCtrl.toDo_single_get)
 router.post("/new", isLoggedIn, todoCtrl.toDo_create_post)
+router.patch("/:id", isLoggedIn, todoCtrl.toDo_update_patch)
+router.delete("/:id", isLoggedIn, todoCtrl.toDo_delete_delete)
 
 module.exports = router
